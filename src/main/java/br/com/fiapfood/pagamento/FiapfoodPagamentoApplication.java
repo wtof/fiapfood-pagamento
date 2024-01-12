610cf2720338ce8aabbb3dd75800290e01c01793
wtof <wt.fox@hotmail.com>
2024-01-12 15:31:36 -0300

    commit inicial

---
+++ src/main/java/br/com/fiapfood/pagamento/FiapfoodPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiapfood.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapfoodPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapfoodPagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiapfood/pagamento/FiapfoodPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiapfood.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapfoodPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
