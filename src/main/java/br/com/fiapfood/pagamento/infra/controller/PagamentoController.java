497c9d2b3d54e8f070c7afec7e295452cb141f99
wtof <wt.fox@hotmail.com>
2024-03-17 11:29:00 -0300

    Tratamento nosniff

--- src/main/java/br/com/fiapfood/pagamento/infra/controller/PagamentoController.java
+++ src/main/java/br/com/fiapfood/pagamento/infra/controller/PagamentoController.java
@@ -3,6 +3,8 @@ package br.com.fiapfood.pagamento.infra.controller;
 import br.com.fiapfood.pagamento.application.interfaces.PagamentoService;
 import br.com.fiapfood.pagamento.application.payload.response.PagamentoResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,9 @@ public class PagamentoController {
     @GetMapping("/pagamento/pedido/{idPedido}")
     @Operation(summary = "Operação responsável por buscar um pagamento por id do pedido")
     public ResponseEntity<PagamentoResponse> buscarPagamentoPorIdPedido(@PathVariable Long idPedido) {
-        return ResponseEntity.ok(pagamentoService.buscarPagamentoPorIdPedido(idPedido));
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Content-Type-Options", "nosniff");
+        PagamentoResponse pagamentoResponse = pagamentoService.buscarPagamentoPorIdPedido(idPedido);
+        return new ResponseEntity<>(pagamentoResponse, headers, HttpStatus.OK);
     }
 }
